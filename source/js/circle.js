7e43a8bce97d2e3428c9766b19bf34c62c79954a
wangmeng <wangmeng08@sparkedu.com>
2023-11-23 15:00:58 +0800

    fix: new cursor, invert color

--- source/js/circle.js
+++ source/js/circle.js
@@ -3,14 +3,16 @@ document.body.appendChild(circle);
 const header = document.querySelector("header");
 circle.classList = ["circle"];
 document.body.insertBefore(circle, header);
+const radius = circle.getBoundingClientRect();
 
 // circle.getClientRects();
 window.addEventListener("mousemove", (e) => {
   //   circle.style.transform = `translate(${e.clientX - 20}px, ${
   //     e.clientY - 20
   //   }px)`;
-  circle.style.top = `${e.clientX - 20}px`;
-  circle.style.left = `${e.clientY - 20}px`;
+
+  circle.style.top = `${e.clientY - radius / 2}px`;
+  circle.style.left = `${e.clientX - radius / 2}px`;
   //   console.log(`translate(${e.clientX}, ${e.clientY})`);
 });
 // window.addEventListener("mouseleave", (e) => {
