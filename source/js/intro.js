bb2d4f40d54c1bc0bc9e6d2f0d210319be039d2e
wangmeng <wangmeng08@sparkedu.com>
2023-11-24 15:36:39 +0800

    fix: change intro

---
+++ source/js/intro.js
@@ -0,0 +1,10 @@
+const intro = document.createElement("div");
+intro.classList = ["intro"];
+
+intro.innerHTML = `
+<h1>Alex Wang</h1>
+<h2>A Software Developer who always wants to be better in tech and life.</h2>
+
+<a>About me</a>
+<a>Github</a>
+`;
