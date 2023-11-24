3575307da96a913aad0cfc7adce3e6562d2e0b2d
wangmeng <wangmeng08@sparkedu.com>
2023-11-24 14:00:53 +0800

    fix: new fixed

--- source/js/circle.js
+++ source/js/circle.js
@@ -3,16 +3,12 @@ document.body.appendChild(circle);
 const header = document.querySelector("header");
 circle.classList = ["circle"];
 document.body.insertBefore(circle, header);
-const radius = circle.getBoundingClientRect();
+const rect = circle.getBoundingClientRect();
 
 // circle.getClientRects();
 window.addEventListener("mousemove", (e) => {
-  //   circle.style.transform = `translate(${e.clientX - 20}px, ${
-  //     e.clientY - 20
-  //   }px)`;
-
-  circle.style.top = `${e.clientY - radius / 2}px`;
-  circle.style.left = `${e.clientX - radius / 2}px`;
+  circle.style.top = `${e.clientY - rect.height / 2}px`;
+  circle.style.left = `${e.clientX - rect.width / 2}px`;
   //   console.log(`translate(${e.clientX}, ${e.clientY})`);
 });
 // window.addEventListener("mouseleave", (e) => {
