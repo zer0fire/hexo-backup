4dbbd5d8f19224bcefa3fdc0f29c4c4511d39c1d
wangmeng <wangmeng08@sparkedu.com>
2023-11-27 15:39:54 +0800

    fix: intro config

--- source/js/intro.js
+++ source/js/intro.js
@@ -1,10 +1,17 @@
-const intro = document.createElement("div");
-intro.classList = ["intro"];
+if (window.location.pathname === "/") {
+  const intro = document.createElement("div");
+  intro.classList = ["intro"];
 
-intro.innerHTML = `
-<h1>Alex Wang</h1>
-<h2>A Software Developer who always wants to be better in tech and life.</h2>
+  intro.innerHTML = `
+    <h1>Alex Wang</h1>
+    <h3>A Software Developer who always wants to be better in tech and life.</h3>
+    
+    <a href="/about">About Me</a>
+    <a href="https://github.com/zer0fire" target="_blank">Github</a>
+    `;
 
-<a>About me</a>
-<a>Github</a>
-`;
+  const container = document.querySelector(
+    ".banner-text.text-center.fade-in-up .h2"
+  );
+  container.appendChild(intro);
+}
