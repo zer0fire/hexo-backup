3d6b76b09022b91d570165ece22a1d3e3f199a43
wangmeng <wangmeng08@sparkedu.com>
2023-11-27 16:10:52 +0800

    fix: delete comment

--- source/js/circle.js
+++ source/js/circle.js
@@ -5,15 +5,7 @@ circle.classList = ["circle"];
 document.body.insertBefore(circle, header);
 const rect = circle.getBoundingClientRect();
 
-// circle.getClientRects();
 window.addEventListener("mousemove", (e) => {
   circle.style.top = `${e.clientY - rect.height / 2}px`;
   circle.style.left = `${e.clientX - rect.width / 2}px`;
-  //   console.log(`translate(${e.clientX}, ${e.clientY})`);
 });
-// window.addEventListener("mouseleave", (e) => {
-//   circle.style.opacity = 0;
-// });
-// window.addEventListener("mouseenter", (e) => {
-//   circle.style.opacity = 255;
-// });
